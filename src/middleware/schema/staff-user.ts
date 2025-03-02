94dc13fac737f8bb526bbd93d29b3a99abf86662
Jyotirmoy Mukhopadhyay <jyotirmoymukhopadhyay2@gmail.com>
2025-03-02 10:57:45 +0530

    file download implemented

--- src/middleware/schema/staff-user.ts
+++ src/middleware/schema/staff-user.ts
@@ -4,6 +4,9 @@ export const createUserValidationSchema = [
   body("name", "Name must not be empty").notEmpty(),
   body("email", "Email must not be empty").notEmpty().isEmail(),
   body("phone_no", "Phone number must be at least 10 characters long").isLength({ min: 10 }),
+  // body("qualification", "Qualification must not be empty").notEmpty(),
+  // body("experience", "Experience must not be empty").notEmpty(),
+  // body("service_id", "service_id must not be empty").notEmpty(),
   body("password", "Experience must not be empty").notEmpty()
 ];
 
--- src/repository/register.ts
+++ src/repository/register.ts
@@ -10,6 +10,9 @@ export default class RegisterRepository {
     name,
     email,
     phone_no,
+    qualification,
+    experience,
+    service_id,
     userType_id,
     salt,
     hash,
@@ -17,8 +20,8 @@ export default class RegisterRepository {
   }: any): Promise<any> {
     try {
       return await sql`
-        INSERT INTO userData (name, email, password, password_salt, phone_no, userType_id, created_by, created_at, updated_by, updated_at)
-      VALUES (${name}, ${email}, ${hash}, ${salt}, ${phone_no}, ${userType_id}, ${created_by || 1}, NOW(), 1, NOW())
+        INSERT INTO userData (name, email, password, password_salt, phone_no,qualification, experience, service_id, userType_id, created_by, created_at, updated_by, updated_at)
+      VALUES (${name}, ${email}, ${hash}, ${salt}, ${phone_no}, ${qualification || null}, ${experience || null}, ${service_id || null}, ${userType_id}, ${created_by || 1}, NOW(), 1, NOW())
       RETURNING *`;
     } catch (error) {
       throw error
